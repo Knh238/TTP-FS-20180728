ddb01e83315ed76bb4599db07bd44969f8e9155d
Kristin <kristinnharper@gmail.com>
2018-11-22 16:28:10 -0600

    got api request to work. Now to think about database type and structure

--- ttp-fullstack/src/client/stock.js
+++ ttp-fullstack/src/client/stock.js
@@ -9,6 +9,12 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import twitter from "../twitterApi";
+import { colors } from "@material-ui/core";
+import axios from "axios";
+// var Twitter = require("twitter-node-client").Twitter;
+// var key = require("....../secrets");
+// var twitter = new Twitter(key);
 
 const styles = {
   card: {
@@ -23,7 +29,58 @@ const styles = {
 };
 
 class Stock extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  async componentWillMount() {
+    // var Twitter = require("twitter");
+
+    // var success = function(data) {
+    //   console.log("Data [%s]", data);
+    // };
+
+    // var error = function(err, response, body) {
+    //   console.log("ERROR [%s]", err);
+    // // };
+    // return async dispatch => {
+    try {
+      const { data } = await axios.get(
+        "https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,tsla&types=quote,news,chart&range=1m&last=5"
+      );
+      console.log("data", data);
+      this.setState(data.AAPL);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  // }
+
+  // twitter.getUser(
+  //   { screen_name: "20pCodependence", count: "10" },
+  //   error,
+  //   success
+  // );
+  // axios.get(
+  //   "https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,tsla&types=quote,news,chart&range=1m&last=5",
+  //   success,
+  //   error
+  // );
+
+  // twitter.getSearch(
+  //   {
+  //     q: " movie -scary :) since:2013-12-27",
+  //     count: 10,
+  //     result_type: "popular"
+  //   },
+
+  //   error,
+  //   success
+  // );
+  // getSearch.header({ "Access-Control-Allow-Origin": "yes" });
+
   render() {
+    console.log("this state", this.state);
     return (
       <Card
         style={{
@@ -44,9 +101,13 @@ class Stock extends React.Component {
           title="cat money"
           fullwidth="true"
         />
-        <CardContent>
-          <Typography variant="display3">Your financial advisor</Typography>
-        </CardContent>
+        {this.state.quote ? (
+          <CardContent>
+            <Typography variant="display3">
+              {this.state.quote.latestPrice}
+            </Typography>
+          </CardContent>
+        ) : null}
       </Card>
     );
   }
